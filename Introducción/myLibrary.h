e60e333c3ef559eab4450e0e653c71523ba2141b
María José Cárdenas <149281799+majo425@users.noreply.github.com>
2024-04-03 19:53:20 -0500

    Create myLibrary.h

---
+++ Introducción/myLibrary.h
@@ -0,0 +1,13 @@
+#ifndef __MYLIBRARY_H__
+#define __MYLIBRARY_H__
+
+/* Función para imprimir matriz */
+void imprimirMatriz (int filas, int columnas);
+
+/* Función para calcular el volumen de un cilindro */
+float volumenCilindro(float radio, float altura);
+
+/* Función para calcular el area de un cuadrado */
+int areaCuadrado (int lado);
+
+#endif
